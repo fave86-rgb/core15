2e7d69cd7c576224885aed7c37a372efed450062
fave86-rgb <fave86@gmail.com>
2026-09-08 07:22:35 +0200

    Force PWA cache refresh for Core 15

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE="core15-v1.1";
+const CACHE="core15-v1.2";
 const ASSETS=["./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting()});
 self.addEventListener("fetch",e=>{
   if(e.request.mode==="navigate"){
-    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return r}).catch(()=>caches.match("./index.html")));
+    e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return r}).catch(()=>caches.match("./index.html")));
   }else{
     e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
   }
